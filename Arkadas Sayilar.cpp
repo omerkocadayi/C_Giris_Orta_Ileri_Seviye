daf87f871e728bc1621ed411809c18615d662800
Ömer Faruk Kocadayı <omerkocadayi@gmail.com>
2017-03-07 22:37:10 +0200

    Update Arkadas Sayilar.cpp

--- Arkadas Sayilar.cpp
+++ Arkadas Sayilar.cpp
@@ -10,19 +10,16 @@
 	Buna gore ikisi de 1000000'dan kucuk arkadas sayi ciftleri nelerdir?   */
 
 #include <stdio.h>
-#define max 1000000
-
-int carpimlar_toplami(int *);
+#define MAX 1000000
+int carpimlar_toplami(int*);
 
 int main(){
 	int a,b;
 	
-	for(a=2;a<max;a++){
-		int atoplam = carpimlar_toplami(&a);
-		for(b=2;b<a;b++){
-			if(atoplam==b && carpimlar_toplami(&b)==a){
-				printf("Bu cift arkadas sayilardir : %6d    %10d \n",a,b);
-			}
+	for(a=2;a<MAX;a++){
+		b = carpimlar_toplami(&a);
+		if(b<a && carpimlar_toplami(&b)==a){    // b<a ifadesini tekrarlara dusmemek icin yazdik..
+  			printf("Bu cift arkadas sayilardir : %6d    %10d \n",a,b);	
 		}
 	}
 	return 0;
