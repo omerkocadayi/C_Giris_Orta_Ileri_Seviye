c2b628782698586e55471497a6c6880bd8fe7a94
Ömer Faruk Kocadayı <omerkocadayi@gmail.com>
2017-03-12 00:48:08 +0200

    Update Girilen cümledeki sesli harfleri bulma.cpp

--- Girilen cümledeki sesli harfleri bulma.cpp
+++ Girilen cümledeki sesli harfleri bulma.cpp
@@ -1,33 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-int main()
 
-{
-	char cumle[100];
+int main(){
+	char cumle[100],deger;
 	char sesliharfler[5]={'a','e','i','o','u'};
 	int sesliharfsayisi=0;
-	char deger;
 	
-	printf("Girmek istediginiz cumle : ");
-    gets(cumle);
+	printf("Girmek istediginiz cumle : "); gets(cumle);
 	
 	int uzunluk=strlen(cumle);
 	
-	for(int i=0;i<uzunluk;i++)
-	{
-		for(int j=0;j<5;j++)
-		{
-			if(cumle[i]==sesliharfler[j])
-			{
+	for(int i=0;i<uzunluk;i++){
+		for(int j=0;j<5;j++){
+			if(cumle[i]==sesliharfler[j]){
 				sesliharfsayisi++;
 				break;
 			}
 		}
 	}
+	
 	printf("\nGirdiginiz cumle : %s",cumle);
 	printf("\nCumlenizdeki sesli harf sayisi : %d", sesliharfsayisi);
-	
-	
 	return 0;
 }
