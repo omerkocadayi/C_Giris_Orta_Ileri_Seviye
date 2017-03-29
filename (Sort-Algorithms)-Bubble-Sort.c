b5ce21e8c6e3216e9dda9dc4c02de46c25fa2204
Ömer Faruk Kocadayı <omerkocadayi@gmail.com>
2017-03-29 16:56:46 +0300

    Add files via upload

---
+++ (Sort-Algorithms)-Bubble-Sort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+void bubble_sort(int [], int);
+
+int main(){
+	int i,k,sayilar[10];
+	
+	for(i=0;i<10;i++){
+		
+		printf("%d. elemani gir : ",i+1); scanf("%d",&sayilar[i]);
+	}
+	
+	bubble_sort(sayilar,i);
+	
+		
+	printf("Dizinin sirali hali \n");
+	
+	for(k=0; k<i; k++){
+		printf("%d\t",sayilar[k]);
+	}
+	
+	return 0;
+}
+
+void bubble_sort(int a[],int n){
+	
+	int temp,i,k;
+
+	for(i=0; i<n; i++){
+	
+		for(k=0; k<n-i-1; k++){
+			
+			if(a[k] > a[k+1]){
+				temp = a[k];
+				a[k] = a[k+1];
+				a[k+1] = temp;
+			}
+		}
+	}
+}
