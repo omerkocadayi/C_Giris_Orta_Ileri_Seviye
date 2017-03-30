4e5dab0c23690341d8adccbf4b2ac68f5ecc3744
Ömer Faruk Kocadayı <omerkocadayi@gmail.com>
2017-03-31 01:32:59 +0300

    Add files via upload

--- Pisagor-Uclusu.c
+++ Pisagor-Uclusu.c
@@ -2,30 +2,22 @@
 Pisagor Uclusu a<b<c ve  a^2 + b^2 = c^2 olarak tanimlanabilir.
 Ornegin ==>  3^2 + 4^2 = 9+16 = 25 = 5^2
 a+b+c = 1000 sonucunu veren, yukaridaki kurala uyan yalnizca 1 adet pisagor uclusu vardir.
-Bu uclu iÃ§in a*b*c kactir ?
+Bu uclu için a*b*c kactir ?
 */
 
 #include <stdio.h>
 
 int main(){
-	int a,a2,b,b2,c,c2;
-	printf("........Pisagor Uclusu Teoremi........ (a^2 + b^2 = c^2)");
+	int a,b,c;
 	
-	for(a=1;a<1000;a++){
-		a2=a*a;
-		
-		for(b=1;b<1000;b++){
-			b2=b*b;
+	for(a=1; a<1000; a++){
+		for(b=1; b<1000; b++){
+			c = 1000-a-b;
 			
-			for(c=1;c<1000;c++){
-				c2=c*c;
-				
-				if(a<b && b<c && (a+b+c)==1000 && c2==a2+b2){
-					printf("\n\n   Toplamlari 1000 yapan pisagor uclusu : %d,%d,%d",a,b,c);
-				   	printf("\n   a^2 + b^2 = c^2  ==> %d + %d = %d",a2,b2,c2);
-					printf("\n   Carpimlari (a*b*c) ==> %d * %d = %d",a,b,(a*b*c));
-					return 0;
-				}
+			if(a*a + b*b == c*c){
+				printf("Bu uclu ==> a = %d , b = %d , c = %d",a,b,c);
+				printf("\nCarpimlari = %d",a*b*c);
+				return 0;
 			}
 		}
 	}
