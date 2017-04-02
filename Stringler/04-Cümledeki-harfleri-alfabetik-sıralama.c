4430b1edc85d39864e45058f7442c27bd338c40b
Ömer Faruk Kocadayı <omerkocadayi@gmail.com>
2017-04-03 02:52:26 +0300

    Update 04-Cümledeki-harfleri-alfabetik-sıralama.c

--- Stringler/04-Cümledeki-harfleri-alfabetik-sıralama.c
+++ Stringler/04-Cümledeki-harfleri-alfabetik-sıralama.c
@@ -2,18 +2,19 @@
 #include <string.h>
 
 int main(){
+	
 	int uzunluk;
 	char gecici,cumle[100];
 	printf("cumleni gir :"); gets(cumle);
 	
 	uzunluk=strlen(cumle);
 	
-     for(int i=0;i<uzunluk;i++){
+	for(int i=0;i<uzunluk;i++){
 	
-     for(int j=0;j<uzunluk;j++){
+		for(int j=0;j<uzunluk;j++){
 			
-      if(cumle[i]<cumle[j]){
-	    gecici=cumle[j];
+			if(cumle[i]<cumle[j]){
+				gecici=cumle[j];
 				cumle[j]=cumle[i];
 				cumle[i]=gecici;
 			}
