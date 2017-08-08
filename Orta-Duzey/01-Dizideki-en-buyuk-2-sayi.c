b2de27ac07f05030a13e48e5663e22825d7597e1
Ömer Faruk Kocadayı <omerkocadayi@gmail.com>
2017-08-08 03:21:41 +0300

    Update 01-Dizideki-en-buyuk-2-sayi.c

--- Orta-Duzey/01-Dizideki-en-buyuk-2-sayi.c
+++ Orta-Duzey/01-Dizideki-en-buyuk-2-sayi.c
@@ -5,20 +5,17 @@ int main(){
 	enb=dizi[0];
 	ikincienb=dizi[1];
 	
-	for(int i=0;i<10;i++){
+	for(int i=0;i<10;i++)
 		printf("%d. degeri giriniz : ",i+1); scanf("%d", &dizi[i]);
-	}
 	
 	for(int i=0;i<10;i++){
-		if(dizi[i]>enb){
+		if(dizi[i]>enb)
 			enb=dizi[i];
-		}
 	}
 	
 	for(int i=0;i<10;i++){
-		if(dizi[i]>ikincienb && dizi[i]!=enb){
+		if(dizi[i]>ikincienb && dizi[i]!=enb)
 			ikincienb=dizi[i];
-		}
 	}
 	
 	printf("\nEn buyuk degeriniz : %d",enb);
