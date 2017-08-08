223a181b71a9f76fc857df191cc0c275faa63f59
Ömer Faruk Kocadayı <omerkocadayi@gmail.com>
2017-08-08 03:20:19 +0300

    Update 24-Dizileri-Birlestirme-(Malloc).c

--- Orta-Duzey/24-Dizileri-Birlestirme-(Malloc).c
+++ Orta-Duzey/24-Dizileri-Birlestirme-(Malloc).c
@@ -11,21 +11,18 @@ int main(){
 	int *sonuc;
 	
 	printf("Dizi1 icin\nAdres\t\tDeger\n");
-	for(int i=0;i<5;i++){
+	for(int i=0;i<5;i++)
 		printf("%x\t\t%d\n",&dizi1[i],dizi1[i]);
-	}
 	
 	printf("\n\nDizi2 icin\nAdres\t\tDeger\n");
-	for(int i=0;i<4;i++){
+	for(int i=0;i<4;i++)
 		printf("%x\t\t%d\n",&dizi2[i],dizi2[i]);
-	}
 	
 	sonuc=birlestir(dizi1, 5, dizi2, 4);
 	
 	printf("\n\n\nDinamik Alan\nAdres\t\tDeger\n");
-	for(int i=0;i<9;i++){
+	for(int i=0;i<9;i++)
 		printf("%x\t\t%d\n",sonuc+i,*(sonuc+i));
-	}
 	
 	return 0;
 }
@@ -36,11 +33,9 @@ int *birlestir(int a[], int n1, int b[], int n2){
 	p=(int *) malloc((n1+n2)*sizeof(int));
 	
 	for(i=0;i<n1;i++)
-		*(p+i)=a[i];
-		
+		*(p+i)=a[i];	
 	for(j=0;j<n2;j++,i++)
 		*(p+i)=b[j];
-		
+	
 	return p;	
-
 }
