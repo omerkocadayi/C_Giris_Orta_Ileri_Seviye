341cbc5258804836f23b68dd704122a2beda173e
Ömer Faruk Kocadayı <omerkocadayi@gmail.com>
2017-08-08 03:29:37 +0300

    Update 08-En-buyuk-cift-ve-en-kucuk-tek-sayilar(Pointer).c

--- Orta-Duzey/08-En-buyuk-cift-ve-en-kucuk-tek-sayilar(Pointer).c
+++ Orta-Duzey/08-En-buyuk-cift-ve-en-kucuk-tek-sayilar(Pointer).c
@@ -8,9 +8,8 @@ int main(){
 	int *p_sayilar=&sayilar[0];
 	kucuk=RAND_MAX;
 	
-	for(int i=0;i<10;i++){
+	for(int i=0;i<10;i++)
 		printf("%d. elemani giriniz : ",i+1); scanf("%d",p_sayilar+i);
-	}
 	
 	buyuk=bul(p_sayilar,&kucuk);
 	printf("\nEn buyuk cift sayiniz : %d\nEn kucuk tek sayiniz : %d",buyuk,kucuk);
@@ -21,16 +20,13 @@ int main(){
 int bul(int *ptr,int *kucuk){
 	int max=0;
 	for(int i=0;i<10;i++){
-		if(*(ptr+i)%2==0 && *(ptr+i)>max){
+		if(*(ptr+i)%2==0 && *(ptr+i)>max)
 			max=*(ptr+i);
-		}
 	}
 	
 	for(int i=0;i<10;i++){
-		if(*(ptr+i) % 2 == 1 && *(ptr+i) < *kucuk){
+		if(*(ptr+i) % 2 == 1 && *(ptr+i) < *kucuk)
 			*kucuk = *(ptr+i);
-		}
 	}
-	
 	return max;
 }
