6b8c94e734d74caed8ca4c19b73832843fd192ad
Ömer Faruk Kocadayı <omerkocadayi@gmail.com>
2017-12-29 05:59:13 +0300

    Update 02 - Bracket_Control(Stack_Array).c

--- Stack and Queue/02 - Bracket_Control(Stack_Array).c
+++ Stack and Queue/02 - Bracket_Control(Stack_Array).c
@@ -5,36 +5,35 @@
  
 typedef struct{
 	char top;
-    char data[STACK_SIZE];
+	char data[STACK_SIZE];
 }stack;
  
 void initialization(stack *stk){
-    stk->top = -1;
+	stk->top = -1;
 }
  
 void push(stack *stk,char c){
-    if(stk->top == STACK_SIZE -1){
-        printf("Stack is full\n");
-     	return;
-    }
-    stk->top++;
-    stk->data[stk->top] = c;
-    printf("Push -- %c\n\n",c);
+	if(stk->top == STACK_SIZE -1){
+		printf("Stack is full\n");
+		return;
+	}
+	stk->top++;
+	stk->data[stk->top] = c;
+	printf("Push -- %c\n\n",c);
 }
  
 char pop(stack *stk){
-    if(stk->top == -1){
-    	printf("Stack is empty\n");
+	if(stk->top == -1){
+		printf("Stack is empty\n");
     }
- 
-    char c = stk->data[stk->top];
-    stk->top--;
-    return c;
+	char c = stk->data[stk->top];
+	stk->top--;
+	return c;
 }
 
 void print_error(){
-     printf("\n\nHatali Parantez Kullanimi Yaptiniz !!!\n\n");
-     exit(1);
+	printf("\n\nHatali Parantez Kullanimi Yaptiniz !!!\n\n");
+	exit(1);
 }
 
 int main(){
@@ -44,13 +43,13 @@ int main(){
 	FILE *fptr;
  
 	if( (fptr = fopen("02 - kontrol.txt","r")) == NULL){
-    	printf("Boyle bir dosya yok\n");
-    	return 0;
+		printf("Boyle bir dosya yok\n");
+		return 0;
 	}
  
     else{
-    	while ( !feof(fptr) ){
-        	key = getc(fptr);
+		while ( !feof(fptr) ){
+			key = getc(fptr);
  
         	switch(key){
             	case '(':
